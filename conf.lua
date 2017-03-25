803d1cf95c093f0c9124d95d0370f5be3bb8f781
Jamie Mansfield <dev@jamierocks.uk>
2017-03-25 22:51:12 +0000

    Basic movement of a rectangle

---
+++ conf.lua
@@ -0,0 +1,3 @@
+function love.conf(t)
+    t.window.title = "My First Love Game"
+end
--- main.lua
+++ main.lua
@@ -1,3 +1,25 @@
+function love.load()
+    player = {}
+    player.x = 100
+    player.y = 100
+end
+
+function love.update()
+    -- y movement
+    if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
+        player.y = player.y - 1
+    elseif love.keyboard.isDown("s") or love.keyboard.isDown("down") then
+        player.y = player.y + 1
+    end
+
+    -- x movement
+    if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
+        player.x = player.x - 1
+    elseif love.keyboard.isDown("d") or love.keyboard.isDown("right") then
+        player.x = player.x + 1
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.rectangle("fill", player.x, player.y, 25, 25)
 end
